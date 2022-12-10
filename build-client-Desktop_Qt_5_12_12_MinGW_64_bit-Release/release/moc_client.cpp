084d67180482b0c5c967a6eb7fc16ec9a10e2356
Nikolas-471 <70167778+Nikolas-471@users.noreply.github.com>
2022-12-10 17:39:02 +0300

    client files release_v

---
+++ build-client-Desktop_Qt_5_12_12_MinGW_64_bit-Release/release/moc_client.cpp
@@ -0,0 +1,162 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'client.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.12)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../client/client.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'client.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.12. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Client_t {
+    QByteArrayData data[11];
+    char stringdata0[165];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Client_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Client_t qt_meta_stringdata_Client = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Client"
+QT_MOC_LITERAL(1, 7, 21), // "enableGetServerButton"
+QT_MOC_LITERAL(2, 29, 0), // ""
+QT_MOC_LITERAL(3, 30, 13), // "sessionOpened"
+QT_MOC_LITERAL(4, 44, 16), // "requestNewServer"
+QT_MOC_LITERAL(5, 61, 13), // "slotReadyRead"
+QT_MOC_LITERAL(6, 75, 9), // "slotError"
+QT_MOC_LITERAL(7, 85, 28), // "QAbstractSocket::SocketError"
+QT_MOC_LITERAL(8, 114, 16), // "slotSendToServer"
+QT_MOC_LITERAL(9, 131, 13), // "slotConnected"
+QT_MOC_LITERAL(10, 145, 19) // "selectImageOnButton"
+
+    },
+    "Client\0enableGetServerButton\0\0"
+    "sessionOpened\0requestNewServer\0"
+    "slotReadyRead\0slotError\0"
+    "QAbstractSocket::SocketError\0"
+    "slotSendToServer\0slotConnected\0"
+    "selectImageOnButton"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Client[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       8,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   54,    2, 0x08 /* Private */,
+       3,    0,   55,    2, 0x08 /* Private */,
+       4,    0,   56,    2, 0x08 /* Private */,
+       5,    0,   57,    2, 0x08 /* Private */,
+       6,    1,   58,    2, 0x08 /* Private */,
+       8,    0,   61,    2, 0x08 /* Private */,
+       9,    0,   62,    2, 0x08 /* Private */,
+      10,    0,   63,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 7,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Client::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Client *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->enableGetServerButton(); break;
+        case 1: _t->sessionOpened(); break;
+        case 2: _t->requestNewServer(); break;
+        case 3: _t->slotReadyRead(); break;
+        case 4: _t->slotError((*reinterpret_cast< QAbstractSocket::SocketError(*)>(_a[1]))); break;
+        case 5: _t->slotSendToServer(); break;
+        case 6: _t->slotConnected(); break;
+        case 7: _t->selectImageOnButton(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 4:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAbstractSocket::SocketError >(); break;
+            }
+            break;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Client::staticMetaObject = { {
+    &QDialog::staticMetaObject,
+    qt_meta_stringdata_Client.data,
+    qt_meta_data_Client,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Client::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Client::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Client.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int Client::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 8)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 8;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 8)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 8;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
